3b4272544fffd2f2fdfb308dc9f4e63141cab68f
LucasCP <ice1life@ice1life@git.tfgamessite.com>
2016-03-21 09:14:51 -0300

    part1-2 pronta indo parte 3

--- t1-part1/chat.py
+++ t1-part1/chat.py
@@ -1,46 +1,65 @@
 from bottle import run, get, post, view, request, redirect
-import thread
 import threading
 import time
 import json
-nserver = 0
-servers = ["localhost:8080"]
+import requests
+servers = ["http://localhost:8080"]
 messages = [("Nobody", "Hello guys!")]
-nick = 'Nobody'
 @get('/')
 @view('index')
 def index():
-    return {'messages': messages,'nick': nick}
+    return {'messages': messages,'nick': ''}
 
 @get('/<nick>')
 @view('index')
 def index(nick):
-    return {'messages': messages,'nick': nick}
+	return {'messages': messages,'nick': nick}
 
 @post('/send')
 def sendMessage():
-	global nick
 	m=request.forms.get('message')
-	n=request.forms.get('nick')
-	if n is not in nick:
-		nick.append(n)
+	n=request.forms.get('nick')	
 	messages.append([n, m])
 	redirect('/'+n)
 
 @get('/peers')
 def getPeers():
 	global servers
-	return servers
-
-@post('/peers/add')
-def addPeer():
-	global nserver
-	global servers
-	for i range (0,nserver)
-		s = request.(servers[i]+'/peers')
-		if s not in servers
-			servers.insert(s)
-	redirect('/')
+	return json.dumps(servers)	
+		
+@get('/peers/msg')
+def getMsg():
+	global messages
+	return json.dumps(messages)
 	
+def clientServ():
+	while True:
+		time.sleep(2)
+		print ('ola!')
+		global servers
+		for i in servers:
+			s = requests.get(i+'/peers')
+			ns = json.loads(s.content.decode("UTF-8"))
+			for j in ns:
+				if ns not in servers:
+					servers.append(j)
 
+def clientMsg():
+	while True:
+		global messages
+		global servers
+		for i in servers:
+			time.sleep(5)
+			s = requests.get(i+'/peers/msg')
+			ns = json.loads(s.content.decode("UTF-8"))
+			if ns not in messages:
+				messages=ns
+				
+	
+threading.Thread(target=clientServ).start()
+
+threading.Thread(target=clientMsg).start()
+	
 threading.Thread(target=run,kwargs=dict(host='localhost', port=8080)).start()
+
+
